4674183ab087739f5f90a8b14e086e892992b9c2
tammyleeseattle <tammylee84@gmail.com>
2018-12-30 21:04:32 -0800

    full psychic game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,94 @@
-/*
+//to ensure that this file runs with the DOM is loaded
+$(document).ready(function() {
 
-Requirements of the Game:
+// GLOBAL VARIABLES
+//--------------------------------------------------------------------------
+    
+    //all possible letters that player or computer can choose from
+    var theAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-The visual elements: display a header, text, Wins, Losses, Guesses Left, and a growing list of Guesses so far
+    //computer randomly chosen letter from the alphabet
+    var computerChoice = "";
 
-- The ability for the game to create a list that is made up of the player keystrokes
-    - this list must increase as the player adds more keystrokes
-    - this list must convert the keystroke to lower case
-    - this list must be displayed to the player
-- Each time a keystroke is found, a -1 is issued from the Guesses Left
-- The ability to store a counter for Wins
-- The ability to store a counter for Losses
-- Create a loop that allows for 9 guesses 
-    - if during the loop, a keystroke matches the computer's random letter, then stop the loop
-        - then add a +1 to the Win counter
-    - if the loop continues until 9 guesses and no match is found, then stop the loop
-        - then add a +1 to the Losses counter
-- Once the loop ends, then reset the Guesses Left and the Guesses So Far
+    //list of player failed attempts to guess the computer's choice
+    var failedGuesses = [];
 
-*/
+    //counter for number of player's successful guesses
+    var winCounter = 0;
 
-//initialize the game by creating counters for wins and losses; setting them to zero
-var wins = 0,
-    losses = 0;
+    //counter for number of player's failed guesses
+    var lossCounter = 0;
 
-//set default max number of guesses to be 9
-var guesses = 9;
+    //maximum number of guesses per "round"
+    var guessesLeft = 9;
 
-//ability for the game to "listen" for player keystrokes
+
+// FUNCTIONS
+//--------------------------------------------------------------------------
+
+// starts a new round
+function newRound () {
+    computerChoice = theAlphabet[Math.floor(Math.random() * theAlphabet.length)];
+
+    // reset the round
+    guessesLeft = 9;
+    failedGuesses = [];
+
+    // change HTML to reflect newRound conditions
+    document.getElementById("guessesLeft").innerHTML = guessesLeft;
+    document.getElementById("winCounter").innerHTML = winCounter;
+    document.getElementById("lossCounter").innerHTML = lossCounter;
+    document.getElementById("failedGuesses").innerHTML = failedGuesses;
+
+    // testing and debugging
+    console.log(computerChoice);
+}
+
+//checks whether player input matches or does not match computer selection
+function checkLetter (letter) {
+
+    //check to see if player's guess matches computer; if so, register win + start new round
+    if (letter === computerChoice) {
+        winCounter++;
+        alert("You guessed my letter! Yay!");
+        document.getElementById("winCounter").innerHTML = winCounter; //update HTML
+        newRound(); // reset the round
+    }
+
+    else if (guessesLeft <= 9 && guessesLeft > 1) {
+        guessesLeft--;
+        failedGuesses.push(letter);
+        document.getElementById("failedGuesses").innerHTML = failedGuesses.join(" ")
+        document.getElementById("guessesLeft").innerHTML = guessesLeft;
+    }
+
+    else {
+        lossCounter++;
+        failedGuesses.push(letter);
+        document.getElementById("failedGuesses").innerHTML = failedGuesses.join(" ")
+        alert("You lost! Try again :)");
+        document.getElementById("lossCounter").innerHTML = lossCounter; // update HTML
+        newRound();
+    }
+
+    // testing and debugging
+    console.log("Win Count: " + winCounter + " | Loss Count: " + lossCounter + " | Guesses Left " + guessesLeft);
+}
+
+//MAIN OPERATIONS
+//--------------------------------------------------------------------------
+
+// initiates the program code for the first round and reset for subsequent rounds
+newRound();
+
+// "listen" for player keystrokes to register them as guesses
 document.onkeyup = function(event) {
-    var playerGuess = event.key; // determines which key was pressed by player
+    var playerGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    checkLetter(playerGuess);
 
-        //create an array of every letter in the alphabet
-        var theAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+        // testing and debugging
+        console.log(playerGuess);
 
-        //ability for computer to randomly select a letter from the alphabet and store it for later use
-        var computerChoice = theAlphabet[Math.floor(Math.random() * theAlphabet.length)];
+}
 
-        //compare player guess vs. computer guess and determine the result
-        function 
+});
